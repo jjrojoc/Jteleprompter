d1f504d4c5c68a15424d179cbca688081ea7d1c5
Juan José Rojo <jjrojoc@gmail.com>
2024-04-14 18:22:33 +0200

    Initial commit
    Initial commit

---
+++ script.js
@@ -0,0 +1,152 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then(function(registration) {
+          console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+          console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+  }
+  
+
+let isAutoScrolling = false;
+let scrollInterval;
+let lastScrollTop = 0;
+
+const teleprompter = document.getElementById('teleprompter');
+const speedControl = document.getElementById('speedControl');
+// Existing variables and toggleAutoScroll function remain the same
+
+const textSizeControl = document.getElementById('textSizeControl');
+const textColorControl = document.getElementById('textColorControl');
+
+document.getElementById('toggleScroll').addEventListener('click', toggleAutoScroll);
+
+
+textSizeControl.addEventListener('input', () => {
+    const newSize = textSizeControl.value + 'px';
+    teleprompter.style.fontSize = newSize;
+});
+
+textColorControl.addEventListener('change', () => {
+    // const newColor = document.getElementById('textColorPicker').value; // toma el color del colorpicker
+    const newColor = textColorControl.value;
+    teleprompter.style.color = newColor;
+});
+
+// Existing speedControl event listener remains the same
+
+
+function toggleAutoScroll() {
+    if (isAutoScrolling) {
+        clearInterval(scrollInterval);
+        document.getElementById('toggleScroll').textContent = 'Start';
+        document.getElementById('toggleScroll').style.backgroundColor = "#007BFF"
+    } else {
+        const speed = 100 - speedControl.value;
+        scrollInterval = setInterval(() => {
+            teleprompter.scrollBy(0, 1);
+            // Update lastScrollTop to new position
+            lastScrollTop = teleprompter.scrollTop;
+        }, speed);
+        document.getElementById('toggleScroll').textContent = 'Stop';
+        document.getElementById('toggleScroll').style.backgroundColor = "#ff0000"
+    }
+    isAutoScrolling = !isAutoScrolling;
+}
+
+
+speedControl.addEventListener('input', () => {
+    if (isAutoScrolling) {
+        clearInterval(scrollInterval);
+        const speed = 100 - speedControl.value;
+        scrollInterval = setInterval(() => {
+            teleprompter.scrollBy(0, 1);
+        }, speed);
+    }
+    
+});
+
+document.getElementById('saveText').addEventListener('click', function() {
+    const scriptText = document.getElementById('teleprompter').innerHTML;
+    localStorage.setItem('savedScript', scriptText);
+    alert('Text saved!');
+});
+
+document.getElementById('loadText').addEventListener('click', function() {
+    const savedText = localStorage.getItem('savedScript');
+    if (savedText) {
+        document.getElementById('teleprompter').innerHTML = savedText;
+        alert('Text loaded!');
+    } else {
+        alert('No saved text found.');
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const savedText = localStorage.getItem('savedScript');
+    if (savedText) {
+        document.getElementById('teleprompter').innerHTML = savedText;
+    }
+});
+
+document.getElementById('changeTextColor').addEventListener('click', function() {
+    const color = document.getElementById('textColorPicker').value;
+    const selection = window.getSelection();
+
+    if (!selection.rangeCount) return;
+
+    const range = selection.getRangeAt(0);
+    const span = document.createElement('span');
+    span.style.color = color;
+    span.appendChild(range.extractContents());
+    range.insertNode(span);
+    selection.removeAllRanges();
+    selection.addRange(range);
+});
+
+
+document.getElementById('editToggle').addEventListener('click', function() {
+    const teleprompter = document.getElementById('teleprompter');
+    const isEditable = teleprompter.contentEditable === "true";
+    teleprompter.contentEditable = !isEditable;  // Toggle the state
+    this.textContent = isEditable ? 'Editar' : 'Parar Editar';  // Update button text
+    if (isEditable){
+    const scriptText = document.getElementById('teleprompter').innerHTML;
+    localStorage.setItem('savedScript', scriptText);
+    alert('Text edited saved!');
+}
+});
+
+document.getElementById('speedControl').addEventListener('input', function() {
+    const speedValueSpan = document.getElementById('scrollSpeedValue');
+    speedValueSpan.textContent = this.value;
+});
+
+document.getElementById('textSizeControl').addEventListener('input', function() {
+    const sizeValueSpan = document.getElementById('textSizeValue');
+    sizeValueSpan.textContent = this.value + 'px';
+});
+
+document.getElementById('menuButton').addEventListener('click', function() {
+    var menuItems = document.getElementById("menuItems");
+    if (menuItems.style.display === "none") {
+        menuItems.style.display = "block";
+    } else {
+        menuItems.style.display = "none";
+    }
+});
+
+// Opcional: Cerrar el menú si se hace clic fuera de él
+window.onclick = function(event) {
+    if (!event.target.matches('#menuButton')) {
+        var dropdowns = document.getElementsByClassName("menu-items");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === "block") {
+                openDropdown.style.display = "none";
+            }
+        }
+    }
+}
