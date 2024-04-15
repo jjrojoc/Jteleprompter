c5416f0692c02e7bd36caa85285a78ab46c497c8
Juan José Rojo <jjrojoc@gmail.com>
2024-04-16 01:28:43 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
 const CACHE_NAME = 'v1';
 const urlsToCache = [
-  '/',
-  'index.html',
-  '/style.css',
-  '/script.js'
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
 ];
 
 self.addEventListener('install', event => {
