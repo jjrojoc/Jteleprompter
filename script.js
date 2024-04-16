098fd48c117dbea17ab51155f0b6a55c82874965
Juan José Rojo <jjrojoc@gmail.com>
2024-04-16 02:14:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/Jteleprompter/service-worker.js')
+      navigator.serviceWorker.register('./service-worker.js')
         .then(function(registration) {
           console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }, function(err) {
