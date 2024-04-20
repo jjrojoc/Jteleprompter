00accfc930af1243c5a449f59df0c48be85e955f
Juan José Rojo <jjrojoc@gmail.com>
2024-04-20 13:57:38 +0200

    test

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_058'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'version_059'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 const URLS = [
   './',
@@ -19,24 +19,35 @@ self.addEventListener('message', (event) => {
   }
 });
 
-// Respond with cached resources
-self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) { // if cache is available, respond with cache
-        console.log('responding with cache : ' + e.request.url)
-        return request
-      } else {       // if there are no cache, try fetching request
-        console.log('file is not cached, fetching : ' + e.request.url)
-        return fetch(e.request)
-      }
+// // Respond with cached resources
+// self.addEventListener('fetch', function (e) {
+//   console.log('fetch request : ' + e.request.url)
+//   e.respondWith(
+//     caches.match(e.request).then(function (request) {
+//       if (request) { // if cache is available, respond with cache
+//         console.log('responding with cache : ' + e.request.url)
+//         return request
+//       } else {       // if there are no cache, try fetching request
+//         console.log('file is not cached, fetching : ' + e.request.url)
+//         return fetch(e.request)
+//       }
 
-      // You can omit if/else for console.log & put one line below like this too.
-      // return request || fetch(e.request)
-    })
-  )
+//       // You can omit if/else for console.log & put one line below like this too.
+//       // return request || fetch(e.request)
+//     })
+//   )
+// });
+
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+      fetch(e.request).catch(function() {
+          return caches.match(e.request).then(function(response) {
+              return response || fetch(e.request);
+          })
+      })
+  );
 });
+
 
 // Cache resources
 self.addEventListener('install', function (e) {
