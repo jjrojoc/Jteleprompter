0350fff61e59f61e234dfe7894c5b2c382af7015
Juan José Rojo <jjrojoc@gmail.com>
2024-04-20 12:30:48 +0200

    test

--- script.js
+++ script.js
@@ -157,7 +157,7 @@ document.getElementById('changeTextColor').addEventListener('click', function() 
     range.insertNode(span);
     selection.removeAllRanges();
     selection.addRange(range);
-    window.removeAllRanges(selection);
+    window.removeAllRanges();
 });
 
 
