1b89a46d17f2da4dd17ef01e610633f4b548ffda
Juan José Rojo <jjrojoc@gmail.com>
2024-04-20 14:13:50 +0200

    test

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_061'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'version_062'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 const URLS = [
   './',
@@ -24,14 +24,14 @@ self.addEventListener('fetch', function (e) {
   console.log('fetch request : ' + e.request.url)
   e.respondWith(
     caches.match(e.request).then(function (request) {
-      if (request) { // if cache is available, respond with cache
-        console.log('responding with cache : ' + e.request.url)
-        return request
-      } else {       // if there are no cache, try fetching request
-        console.log('file is not cached, fetching : ' + e.request.url)
-        return fetch(e.request)
-      }
-
+      // if (request) { // if cache is available, respond with cache
+      //   console.log('responding with cache : ' + e.request.url)
+      //   return request
+      // } else {       // if there are no cache, try fetching request
+      //   console.log('file is not cached, fetching : ' + e.request.url)
+      //   return fetch(e.request)
+      // }
+      fetch(e.request) || request
       // You can omit if/else for console.log & put one line below like this too.
       // return request || fetch(e.request)
     })
