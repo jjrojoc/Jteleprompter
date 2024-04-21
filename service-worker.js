fa2a1623fa36ff3b54e18f2343b941705a074507
Juan José Rojo <jjrojoc@gmail.com>
2024-04-21 10:20:41 +0200

    change default value for settings

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_113'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'version_114'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 const URLS = [
   './',
   './manifest.json',
-  './index.html?v=113',
-  './style.css?v=113',
-  './script.js?v=113',
+  './index.html?v=114',
+  './style.css?v=114',
+  './script.js?v=114',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png',
   './icons/favicon.ico',
