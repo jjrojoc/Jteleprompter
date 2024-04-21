36faedffadfc0de45234d6b690c29574066ecbb4
Juan José Rojo <jjrojoc@gmail.com>
2024-04-21 10:27:50 +0200

    change label in settings

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_114'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'version_115'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 const URLS = [
   './',
   './manifest.json',
-  './index.html?v=114',
-  './style.css?v=114',
-  './script.js?v=114',
+  './index.html?v=115',
+  './style.css?v=115',
+  './script.js?v=115',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png',
   './icons/favicon.ico',
