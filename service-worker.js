144b942d73bbd41d75bb1a5a6d14989737b9dd43
Juan José Rojo <jjrojoc@gmail.com>
2024-04-23 11:30:30 +0200

    esquinas redondeadas a timer

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_236'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'version_237'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 const URLS = [
   './',
   './manifest.json',
-  './index.html?v=236',
-  './style.css?v=236',
-  './script.js?v=236',
+  './index.html?v=237',
+  './style.css?v=237',
+  './script.js?v=237',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png',
   './icons/favicon.ico',
