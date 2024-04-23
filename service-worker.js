3f9ef9ef17ab0f3d7cbfc6cbdebd08446baccadb
Juan José Rojo <jjrojoc@gmail.com>
2024-04-24 01:16:01 +0200

    desactivar zoom pantalla con dedos y aumentado tamaño cronometro al doble cuando presionado

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_262'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'version_263'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 const URLS = [
   './',
   './manifest.json',
-  './index.html?v=262',
-  './style.css?v=262',
-  './script.js?v=262',
+  './index.html?v=263',
+  './style.css?v=263',
+  './script.js?v=263',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png',
   './icons/favicon.ico',
