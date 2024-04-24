132b102340d07b69e2e4a5e9f1e13a72f17ab66e
Juan José Rojo <jjrojoc@gmail.com>
2024-04-24 13:28:45 +0200

    menos tamaño para color picker

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_280'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'version_281'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 const URLS = [
   './',
   './manifest.json',
-  './index.html?v=280',
-  './style.css?v=280',
-  './script.js?v=280',
+  './index.html?v=281',
+  './style.css?v=281',
+  './script.js?v=281',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png',
   './icons/favicon.ico',
