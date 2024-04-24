bd7437b4263332b83d7739fbcf19616095005411
Juan José Rojo <jjrojoc@gmail.com>
2024-04-24 09:53:08 +0200

    test timer scale 1.4

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_266'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'version_267'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 const URLS = [
   './',
   './manifest.json',
-  './index.html?v=266',
-  './style.css?v=266',
-  './script.js?v=266',
+  './index.html?v=267',
+  './style.css?v=267',
+  './script.js?v=267',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png',
   './icons/favicon.ico',
