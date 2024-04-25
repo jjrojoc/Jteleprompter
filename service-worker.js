d32ea4e8ee03231b448a8a97264b4c9f637f5052
Juan José Rojo <jjrojoc@gmail.com>
2024-04-25 23:16:56 +0200

    fix

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_329'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'version_330'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 const URLS = [
   './',
   './manifest.json',
-  './index.html?v=329',
-  './style.css?v=329',
-  './script.js?v=329',
+  './index.html?v=330',
+  './style.css?v=330',
+  './script.js?v=330',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png',
   './icons/favicon.ico',
