e3ec70ee302afb903e7e3619048d7a45267210be
Juan José Rojo <jjrojoc@gmail.com>
2024-04-26 10:23:56 +0200

    fix

--- script.js
+++ script.js
@@ -273,7 +273,7 @@ function handlePressUp() {
     // Limpiar los temporizadores y revertir cambios visuales si el botón es soltado antes de 3 segundos
     clearTimeout(pressTimer);
     clearTimeout(visualTimer);
-    toggleButton.style.backgroundColor = "blue"; // Color original
+    toggleButton.style.backgroundColor = (255, 255, 255, 0.2); // Color original
 }
 
 function activateSpecialFunction() {
--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_338'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'version_339'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 const URLS = [
   './',
   './manifest.json',
-  './index.html?v=338',
-  './style.css?v=338',
-  './script.js?v=338',
+  './index.html?v=339',
+  './style.css?v=339',
+  './script.js?v=339',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png',
   './icons/favicon.ico',
