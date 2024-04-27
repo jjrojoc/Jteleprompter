d9a942af406a0fceafcb79a790943bbeae3f57b9
Juan José Rojo <jjrojoc@gmail.com>
2024-04-27 07:06:22 +0200

    mover textcolorcontrol a menubar

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_372'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'version_373'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 const URLS = [
   './',
   './manifest.json',
-  './index.html?v=372',
-  './style.css?v=372',
-  './script.js?v=372',
+  './index.html?v=373',
+  './style.css?v=373',
+  './script.js?v=373',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png',
   './icons/favicon.ico',
