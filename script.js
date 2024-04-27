0825d955f701edd4504cf4eeecbe0621efedb1fe
Juan José Rojo <jjrojoc@gmail.com>
2024-04-27 21:41:26 +0200

    fix, ahora sí, función paste bastante lograda

--- script.js
+++ script.js
@@ -888,7 +888,7 @@ document.getElementById('teleprompter').addEventListener('paste', function(e) {
             if (node.style) {
                 // Cambiar el color negro a blanco, dejar otros colores intactos
                 if (node.style.color === 'black' || node.style.color === 'rgb(0, 0, 0)') {
-                    node.style.color = 'white';
+                    node.style.color = '';
                 }
                 // Eliminar estilos no necesarios
                 node.style.fontSize = '';
