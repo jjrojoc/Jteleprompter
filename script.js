7467686c7a5b29cd0df0caf0764425fa58db069d
Juan José Rojo <jjrojoc@gmail.com>
2024-04-27 22:26:05 +0200

    fix

--- script.js
+++ script.js
@@ -887,7 +887,7 @@ document.getElementById('teleprompter').addEventListener('paste', function(e) {
         Array.from(element.querySelectorAll('*')).forEach(node => {
             if (node.style) {
                 // Cambiar el color negro a blanco, dejar otros colores intactos
-                if (node.style.color === 'black' || node.style.color === 'white' || node.style.color === 'rgb(255, 255, 255)') {
+                if (node.style.color === 'black' || node.style.color === 'white' || node.style.color === "rgb(255, 255, 255)") {
                     node.style.color = 'none';
                 }
                 // Eliminar estilos no necesarios
--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_412'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'version_413'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 const URLS = [
   './',
   './manifest.json',
-  './index.html?v=412',
-  './style.css?v=412',
-  './script.js?v=412',
+  './index.html?v=413',
+  './style.css?v=413',
+  './script.js?v=413',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png',
   './icons/favicon.ico',
