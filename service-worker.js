7cbbc4b9855b2768fd34d6f3be63853f3939617a
Juan José Rojo <jjrojoc@gmail.com>
2024-04-27 07:02:35 +0200

    mover changetextcolor a menubar

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_371'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'version_372'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 const URLS = [
   './',
   './manifest.json',
-  './index.html?v=371',
-  './style.css?v=371',
-  './script.js?v=371',
+  './index.html?v=372',
+  './style.css?v=372',
+  './script.js?v=372',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png',
   './icons/favicon.ico',
