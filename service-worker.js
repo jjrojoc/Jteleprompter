31bccfb140a56c2d3a90122ca0129baaaceb9aab
Juan José Rojo <jjrojoc@gmail.com>
2024-04-28 11:40:54 +0200

    fix quitar etiqueta C:

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_444'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'version_445'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 const URLS = [
   './',
   './manifest.json',
-  './index.html?v=444',
-  './style.css?v=444',
-  './script.js?v=444',
+  './index.html?v=445',
+  './style.css?v=445',
+  './script.js?v=445',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png',
   './icons/favicon.ico',
