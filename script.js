3ed68cc7879cfeedb2e6cc3c8051792626ee39b9
Juan José Rojo <jjrojoc@gmail.com>
2024-04-28 15:31:20 +0200

    test clacular duración estimada

--- script.js
+++ script.js
@@ -1121,6 +1121,7 @@ function prepareTeleprompter() {
     const endMarker = document.getElementById("endMarker");
     endMarker.addEventListener('touchstart', handleEndMarkerTouch, { passive: true });
     endMarker.addEventListener('click', handleEndMarkerTouch); // Para dispositivos no táctiles
+    estimateDuration();
 }
 
 
@@ -1296,3 +1297,23 @@ teleprompter.addEventListener('blur', function() {
 
 // Inicializa el placeholder correctamente al cargar
 document.addEventListener('DOMContentLoaded', setPlaceholder);
+
+
+var speedControl = document.getElementById('speedControl');  // Control de velocidad del usuario
+var teleprompter = document.getElementById("teleprompter");  // Elemento del teleprompter
+
+function estimateDuration() {
+    var height = teleprompter.scrollHeight; // Altura total del contenido del teleprompter
+    var speed = parseInt(speedControl.value, 10); // Velocidad del scroll, normalmente un valor que el usuario puede ajustar
+    var freq = 250; // Frecuencia en ms con la que se actualiza el scroll
+    var gap = 1; // Cantidad de píxeles que se mueve el contenido por intervalo de tiempo
+
+    var interval = Math.round(freq / speed); // Calcula el tiempo entre cada movimiento de scroll
+    var duration = height / (gap / interval) / 1000; // Duración total estimada para recorrer todo el contenido
+    var date = new Date(null);
+    date.setSeconds(Math.round(duration)); // Configura los segundos calculados en un objeto Date
+    var result = date.toISOString().substr(11, 8); // Formatea el tiempo en HH:MM:SS
+
+    document.getElementById("durationContainer").innerHTML = result; // Muestra el resultado en la interfaz de usuario
+    return duration;
+}
