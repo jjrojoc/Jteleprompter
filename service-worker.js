349e3f50304901f52388f6c3e45063b8d5959b1a
Juan José Rojo <jjrojoc@gmail.com>
2024-04-29 11:31:02 +0200

    fix vuelta a 0.30 para calcular el tiempo estimado

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_491'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'version_492'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 const URLS = [
   './',
   './manifest.json',
-  './index.html?v=491',
-  './style.css?v=491',
-  './script.js?v=491',
+  './index.html?v=492',
+  './style.css?v=492',
+  './script.js?v=492',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png',
   './icons/favicon.ico',
