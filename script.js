168bedbd7cc0cc89f3cccad31fe649bdc485d359
Juan José Rojo <jjrojoc@gmail.com>
2024-05-01 16:40:58 +0200

    fix

--- script.js
+++ script.js
@@ -298,7 +298,7 @@ let updateDurationInterval; // Guarda el ID del intervalo para poder detenerlo m
 function startAutoScroll() {
     const teleprompter = document.getElementById('teleprompter');
     const speedControl = document.getElementById('speedControl');
-    var speedPerPixel = (100 - speedControl.value) * 1.5; // Ajusta este valor según la realidad del desplazamiento
+    var speed = (100 - speedControl.value) * 1.5; // Ajusta este valor según la realidad del desplazamiento
     // const speed = 100 - speedControl.value;
     
     isAutoScrolling = true;
