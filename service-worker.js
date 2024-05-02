ac56133a40d3a4d12e18b7c6368e757a9e07358e
Juan José Rojo <jjrojoc@gmail.com>
2024-05-02 09:49:40 +0200

    fix, pixelacumulator para que no pare cuando es menos de 0,5

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_636'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'version_637'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 const URLS = [
   './',
   './manifest.json',
-  './index.html?v=636',
-  './style.css?v=636',
-  './script.js?v=636',
+  './index.html?v=637',
+  './style.css?v=637',
+  './script.js?v=637',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png',
   './icons/favicon.ico',
