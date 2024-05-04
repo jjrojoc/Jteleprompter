0cccabd052cfeda98ed3ed21e2dea16df5280ca2
Juan José Rojo <jjrojoc@gmail.com>
2024-05-04 07:54:38 +0200

    fix translatey no muestra texto que no estaba en pantalla al iniciar la animación

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_685'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'version_686'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 const URLS = [
   './',
   './manifest.json',
-  './index.html?v=685',
-  './style.css?v=685',
-  './script.js?v=685',
+  './index.html?v=686',
+  './style.css?v=686',
+  './script.js?v=686',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png',
   './icons/favicon.ico',
