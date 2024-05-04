c606a2eaad245413252c3da4406f5209605135fa
Juan José Rojo <jjrojoc@gmail.com>
2024-05-04 19:20:36 +0200

    fix, volver a teleprompter original

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_736'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'version_737'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 const URLS = [
   './',
   './manifest.json',
-  './index.html?v=736',
-  './style.css?v=736',
-  './script.js?v=736',
+  './index.html?v=737',
+  './style.css?v=737',
+  './script.js?v=737',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png',
   './icons/favicon.ico',
