5b52f683986d4b89c5b6ca6c2172037e151dc731
Juan José Rojo <jjrojoc@gmail.com>
2024-05-04 20:45:24 +0200

    fix

--- script.js
+++ script.js
@@ -446,7 +446,8 @@ function startAutoScroll() {
     toggleControlsDisplay(false);
 
     if (translateYValue === 0) {
-        teleprompter.style.transform = 'translateY(calc(100vh))';
+        translateYValue = window.innerHeight; // Empieza justo debajo de la pantalla
+        teleprompter.style.transform = `translateY(${translateYValue}px)`;
         cronometro.reset();
         cronometro.start();
     } else {
