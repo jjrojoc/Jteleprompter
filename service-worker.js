9ed180009e576435978fd2c7c07fe7cefd88775d
Juan José Rojo <jjrojoc@gmail.com>
2024-05-04 13:03:57 +0200

    endmarker se muestra completo al terminar

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_714'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'version_715'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 const URLS = [
   './',
   './manifest.json',
-  './index.html?v=714',
-  './style.css?v=714',
-  './script.js?v=714',
+  './index.html?v=715',
+  './style.css?v=715',
+  './script.js?v=715',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png',
   './icons/favicon.ico',
