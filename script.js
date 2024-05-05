9472dc4986dd806b9b5fe02de6c5ad7fc9b874d9
Juan José Rojo <jjrojoc@gmail.com>
2024-05-05 04:02:18 +0200

    fix

--- script.js
+++ script.js
@@ -444,50 +444,51 @@ function startAutoScroll() {
 
     if (!isAutoScrolling) {
         isAutoScrolling = true;
+        translateYValue = window.innerHeight;  // Asegura posición inicial correcta
+        teleprompter.style.transform = `translateY(${translateYValue}px)`;
+
         updateToggleButton(true);
         toggleControlsDisplay(false);
 
-        if (translateYValue === 0) {
-            // Ajuste inicial para asegurar que todo el contenido sea visible
-            teleprompter.style.height = `${teleprompter.scrollHeight}px`;
-            // Coloca inicialmente el contenido justo por debajo de la pantalla
-            translateYValue = window.innerHeight;
-            teleprompter.style.transform = `translateY(${translateYValue}px)`;
-            cronometro.reset();
-            cronometro.start();
-        }   else {
-            cronometro.start();
-        }
-        
-        startEstimatedTimeCountdown();
+        lastTime = null;  // Restablecer el tiempo inicial para la animación
 
-        let lastTime;
         function animateScroll(timestamp) {
-            if (!lastTime) {
+            if (lastTime === null) {
                 lastTime = timestamp;
             }
-
             const deltaTime = timestamp - lastTime;
             lastTime = timestamp;
 
             translateYValue -= (pixelsPerSecond * deltaTime) / 1000;
-
-            const endMarkerRect = document.getElementById("endMarker").getBoundingClientRect();
-
-            // Detiene el desplazamiento si el marcador final ha pasado por completo la parte inferior de la pantalla
-            if (endMarkerRect.bottom <= window.innerHeight -100) {
+            if (translateYValue <= 0) {
+                translateYValue = 0;  // Evita valores negativos
+                teleprompter.style.transform = `translateY(0px)`;
                 stopAutoScroll();
             } else {
                 teleprompter.style.transform = `translateY(${translateYValue}px)`;
-                scrollAnimation = requestAnimationFrame(animateScroll);
+                requestAnimationFrame(animateScroll);
             }
         }
 
-        scrollAnimation = requestAnimationFrame(animateScroll);
+        requestAnimationFrame(animateScroll);
+        startEstimatedTimeCountdown();
     } else {
-        console.log('Attempt to start auto-scroll but it is already running.');
+        console.log('Intento de iniciar el auto-scroll pero ya está en ejecución.');
     }
 }
+
+function stopAutoScroll() {
+    if (isAutoScrolling) {
+        isAutoScrolling = false;
+        cancelAnimationFrame(scrollAnimation);
+        scrollAnimation = null;
+        updateToggleButton(false);
+        toggleControlsDisplay(true);
+        stopEstimatedTimeCountdown();
+        console.log("Auto-scroll detenido.");
+    }
+}
+
 
     
 function stopAutoScroll() {
