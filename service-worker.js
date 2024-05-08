29b36b9a9b0c620c246da3bec82fbdb86ac2b0eb
Juan José Rojo <jjrojoc@gmail.com>
2024-05-08 14:44:54 +0200

    Test fuente en bold por defecto

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_936'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'version_937'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 const URLS = [
   './',
   './manifest.json',
-  './index.html?v=936',
-  './style.css?v=936',
-  './script.js?v=936',
+  './index.html?v=937',
+  './style.css?v=937',
+  './script.js?v=937',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png',
   './icons/favicon.ico',
