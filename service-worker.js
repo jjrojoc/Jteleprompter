0671db0561c88e47e13df4bd7843a6c5a8b61b77
Juan José Rojo <jjrojoc@gmail.com>
2024-05-09 10:08:36 +0200

    update fix para cambio de color

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_953'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'version_954'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 const URLS = [
   './',
   './manifest.json',
-  './index.html?v=953',
-  './style.css?v=953',
-  './script.js?v=953',
+  './index.html?v=954',
+  './style.css?v=954',
+  './script.js?v=954',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png',
   './icons/favicon.ico',
