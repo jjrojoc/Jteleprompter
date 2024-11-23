6b0ed5dc569be35fac41f1e5b6468fd632b6f00c
Juan José Rojo <jjrojoc@gmail.com>
2024-11-23 10:24:03 +0100

    Undoing gapi changes, will be a new project

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_1094'              // Version of the off-line cache (change this value everytime you want to update cache)
+var VERSION = 'version_1095'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 const URLS = [
   './',
   './manifest.json',
-  './index.html?v=1094',
-  './style.css?v=1094',
-  './script.js?v=1094',
+  './index.html?v=1095',
+  './style.css?v=1095',
+  './script.js?v=1095',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png',
   './icons/favicon.ico',
